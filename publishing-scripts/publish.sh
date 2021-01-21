d908d19e84088d2c5ea0673d554fdeeefaee7411
David Salgado <david@digitalronin.com>
2021-01-21 16:00:37 +0900

    Set ROOT_DOCPATH before calling `build`
    This env. var will always have the same value as
    the config setting for `service_link`, so we don't
    need to ask the user to set it separately.
    
    This means the publishing github action can be
    identical for all documentation sites that use
    this system.

--- publishing-scripts/publish.sh
+++ publishing-scripts/publish.sh
@@ -19,6 +19,10 @@ main() {
 }
 
 compile_html() {
+  # Export the ROOT_DOCPATH env. var which is required by the patched tech-docs
+  # gem code
+  export ROOT_DOCPATH=$(site_root)
+
   bundle exec middleman build --build-dir docs --relative-links
   touch docs/.nojekyll
 }
