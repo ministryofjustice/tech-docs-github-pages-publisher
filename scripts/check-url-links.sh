bee3ba524126e244230ccd4cbd26818a15cf60dc
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-03-03 12:09:16 +0000

    Update htmlproofer

--- scripts/check-url-links.sh
+++ scripts/check-url-links.sh
@@ -9,5 +9,10 @@ cp /stashed-files/* .
 # Compile source markdown files into HTML in the `/docs` directory
 bundle exec middleman build --build-dir docs --relative-links --verbose
 
-# Check all URLs 
-htmlproofer --log-level debug --allow-missing-href true --typhoeus '{"connecttimeout": 10, "timeout": 30, "accept_encoding": "zstd,br,gzip,deflate" }' ./docs
+# Check all URLs apart from `search` as that is created as part of the build 
+htmlproofer \
+  --log-level debug \
+  --allow-missing-href true \
+  --typhoeus '{"connecttimeout": 10, "timeout": 30, "accept_encoding": "zstd,br,gzip,deflate" }' \
+  --ignore-files "/.\/docs\/search\/index.html/" \
+  ./docs
