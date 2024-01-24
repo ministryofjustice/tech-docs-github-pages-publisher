b6f56ded0b80491d2314943d0133b20c65d96068
jaskaransarkaria <jaskaran.sarkaria@digital.justice.gov.uk>
2024-01-24 15:31:24 +0000

    feat: 🎸 allow `check_interal_hash` to be overridden

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,8 @@
 set -x
 set -euo pipefail
 
+CHECK_INTERNAL_HASH=${1:-true}
+
 # Restore the stashed config.rb Gemfile and Gemfile.lock
 cp /stashed-files/* .
 
@@ -12,6 +14,6 @@ bundle exec middleman build --build-dir docs --relative-links --verbose
 touch docs/.nojekyll
 
 # Internal link check only within the docs folder
-htmlproofer --log-level debug --allow-missing-href true --disable_external true ./docs
+htmlproofer --log-level debug --allow-missing-href true --disable_external true --check-internal-hash "$CHECK_INTERNAL_HASH"  ./docs
 
 tar --dereference --directory docs -cvf artifact.tar --exclude=.git --exclude=.github .
